76e5cfabf9a95401758eb37830370e08d9ea4dc4
Jino42 <ntoniolo@student.42.fr>
2019-04-02 18:00:13 +0200

    Orgasmic display shell

--- incs/Grid.cpp
+++ incs/Grid.cpp
@@ -5,7 +5,11 @@ std::ostream &operator<<(std::ostream &os, const Grid<ValuePuzzle > &grid) {
 	for (int index = 0; index < grid.size(); ++index) {
 		if (!(index % grid.getX()))
 			os << std::endl;
+		if (!grid.data_[index])
+			os << "\033[31m";
+		os << std::setw(3);
 		os << grid.data_[index] << ' ';
+		os << "\033[0m";
 	}
 	os << std::endl;
 	return os;
